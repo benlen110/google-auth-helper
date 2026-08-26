2f60ccedfa4857492a5806decad402beabf3e383
benlen110 <zoiksyourface@gmail.com>
2026-08-25 17:25:55 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Public configuration only. Never add a Google client secret here.
 window.GOOGLE_AUTH_CONFIG = Object.freeze({
-  clientId: "REPLACE_WITH_GOOGLE_WEB_CLIENT_ID.apps.googleusercontent.com",
+  clientId: "224112490794-72mu6he9gdml6kr92vkse3oqnn8vhqs5.apps.googleusercontent.com",
   allowedOpenerOrigins: Object.freeze([
     "https://REPLACE_WITH_ITCH_IFRAME_ORIGIN"
   ])
